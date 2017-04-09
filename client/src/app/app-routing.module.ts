24e0bb969a56917d6775a631db3f28ffcb8cff86
panicdragon <d.takayama.web@gmail.com>
2017-04-09 22:29:05 +0900

    ng g component user & add routing

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UserComponent,
+    children: []
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
