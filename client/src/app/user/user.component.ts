0fa07a7b5e0916ba87436cbce8c39aaae1153758
panicdragon <d.takayama.web@gmail.com>
2017-04-10 00:26:25 +0900

    create UserService

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
+import { User } from '../user';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [UserService]
 })
 export class UserComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
-  ngOnInit() {
-
+  getUser(): void {
+    this.userService.getUser()
+      .subscribe((users) => {
+        this.users = users;
+      });
   }
 
+  ngOnInit() {
+    this.getUser();
+  }
 }
