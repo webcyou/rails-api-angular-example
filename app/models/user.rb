6190826068720489d91a38a8d17155d49c8863de
panicdragon <d.takayama.web@gmail.com>
2017-05-20 23:17:59 +0900

    rails change db mysql & devise_token_auth setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :rememberable, :trackable, :omniauthable,
+         :validatable #,:recoverable,:confirmable
+  include DeviseTokenAuth::Concerns::User
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'auth'
+
+  resources :private_resource, only: :index
 end
